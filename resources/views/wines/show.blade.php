be5f6f0f7e6dc87eb8d52d6dffa6549d68cd247a
alejandra-garcias <alejandra.garsanchez@gmail.com>
2023-09-29 13:27:54 +0200

    wine.show.updated

--- resources/views/wines/show.blade.php
+++ resources/views/wines/show.blade.php
@@ -3,69 +3,68 @@
 @section('title', 'Ver wine')
 
 @section('content')
-    <h1 class="text-center">Show wine</h1>
-    <div class="row d-flex justify-content-center">
+    <div class="wine-show-container">
+        <div class="wine-picture-container">
+            <img src="{{ $wine->image }}" alt="{{ $wine->name }}">
+        </div>
+        <div class="wine-description">
+            <div class="wine-info-card">
+                <div class="no-currency">
+                    <h1>{{ $wine->name }}</h1>
+                    <p>{{ $wine->description }}</p>
+                    <p>
+                        @foreach ($wine->bars as $bar)
+                            <span class="badge text-bg-primary">
+                                <a href="{{ route('bars.show', $bar) }}">{{ $bar->name }}</a></span>
+                        @endforeach
+                    </p>
 
-        <div class="col-3 py-2 ">
-            <div class="card" style="width: 18rem;">
-                <img src="{{ $wine->image }}" class="card-img-top" alt="{{ $wine->name }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $wine->name }}</h5>
-                        <p class="card-text">{{ $wine->description }}</p>
-                        <p class="card-text">
-                            @foreach ($wine->bars as $bar)
-                                <span class="badge text-bg-primary">
-                                    <a href="{{ route('bars.show', $bar) }}" class="nav-link">{{ $bar->name }}</a></span>
-                            @endforeach
-                        </p>
-                        <p class="card-text">{{ $wine->winery }}</p>
-                        <p class="card-text">{{ $wine->price }} $</p>
-                        @isset($rates)
-                            <a href="javascript:verConversiones()" class='btn btn-primary' alt='Show rates'><i
-                                    class="bi bi-caret-down-fill"></i></a>
-                            <div id='divConversiones' style='display:none'>
-                                @foreach ($rates as $key => $rate)
-                                    <br>{{ $key . '' . $rate }}
-                                @endforeach
-                            </div>
+                    <p><span class="badge text-bg-primary">Winery</span> {{ $wine->winery }}</p>
+                    <p><span class="badge text-bg-primary">Vol%</span> {{ $wine->vol }}% </p>
+                </div>
+                <div class="wine-buttons">
+                    @auth
 
-                        @endisset
+                        <form method='POST' action="{{ route('wine.destroy', $wine->id) }}"
+                            onsubmit="return confirmar ('Are you sure you would like to delete this wine?','Notification');">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="btn btn-primary">Delete</button>
+                        </form>
 
+                        <a href="{{ route('wine.edit', $wine->id) }}" class="btn btn-primary">Edit</a>
+                    @endauth
+                    <a href="{{ route('wine.index') }}" class="btn btn-primary">Return</a>
 
-                        <p class="card-text">{{ $wine->vol }}% </p>
-
-
-
-                    </div>
                 </div>
             </div>
+            <div class="currency">
+                <p><span class="badge text-bg-primary">Price</span> {{ $wine->price }} $</p>
+                @isset($rates)
+                    <a href="javascript:verConversiones()" alt='Show rates'><img src="/img/currency.svg" alt=""></a>
+                    <div class="conversion"id='divConversiones' style='display:none;'>
+                        @foreach ($rates as $key => $rate)
+                            <br>{{ $key . '->' . $rate }}
+                        @endforeach
+                    </div>
 
+                @endisset
+            </div>
         </div>
-        <div class="d-flex justify-content-center">
-            @auth
 
-                <form method='POST' action="{{ route('wine.destroy', $wine->id) }}"
-                    onsubmit="return confirmar ('Are you sure you would like to delete this wine?','Notification');">
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit" class="btn btn-primary">Delete</button>
-                </form>
 
-                <a href="{{ route('wine.edit', $wine->id) }}" class="btn btn-primary">Edit</a>
-            @endauth
-            <a href="{{ route('wine.index') }}" class="btn btn-primary">Return</a>
+    </div>
 
-        </div>
-        <script>
-            function verConversiones() {
-                let obj = document.getElementById('divConversiones');
-                if (obj) {
-                    if (obj.style.display == 'none') {
-                        obj.style.display = 'block';
-                    } else {
-                        obj.style.display = 'none';
-                    }
+    <script>
+        function verConversiones() {
+            let obj = document.getElementById('divConversiones');
+            if (obj) {
+                if (obj.style.display == 'none') {
+                    obj.style.display = 'block';
+                } else {
+                    obj.style.display = 'none';
                 }
             }
-        </script>
-    @endsection
+        }
+    </script>
+@endsection
