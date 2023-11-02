dcb57039a280cbd26779a21055847469c09ba8f6
alejandra-garcias <alejandra.garsanchez@gmail.com>
2023-11-02 21:14:41 +0100

    test

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -30,7 +30,7 @@
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link text-beige" href="{{route('bars.index')}}">Bar Selection</a>
+                        <a class="nav-link text-beige" href="{{route('bars.index')}}">Bar    Selection</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-beige" href="{{route('wine.index')}}">Wine Selection</a>
